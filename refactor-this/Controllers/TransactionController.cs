33016df75198cbf386389e94977e5a4268c37838
Ynel Basa <marinelbasa@gmail.com>
2020-12-08 20:32:38 +1300

    Move database operations from controller and model to TransactionRepository

--- refactor-this/Controllers/TransactionController.cs
+++ refactor-this/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
 ﻿using refactor_this.Models;
+using refactor_this.Repository;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -11,42 +12,20 @@ namespace refactor_this.Controllers
 {
     public class TransactionController : ApiController
     {
+        private readonly TransactionRepository _transactionRepository = new TransactionRepository();
+
         [HttpGet, Route("api/Accounts/{id}/Transactions")]
         public IHttpActionResult GetTransactions(Guid id)
         {
-            using (var connection = Helpers.NewConnection())
-            {
-                SqlCommand command = new SqlCommand($"select Amount, Date from Transactions where AccountId = '{id}'", connection);
-                connection.Open();
-                var reader = command.ExecuteReader();
-                var transactions = new List<Transaction>();
-                while (reader.Read())
-                {
-                    var amount = (float)reader.GetDouble(0);
-                    var date = reader.GetDateTime(1);
-                    transactions.Add(new Transaction(amount, date));
-                }
-                return Ok(transactions);
-            }
+            var transactions = _transactionRepository.GetTransactions(id);
+            return Ok(transactions);    
         }
 
         [HttpPost, Route("api/Accounts/{id}/Transactions")]
         public IHttpActionResult AddTransaction(Guid id, Transaction transaction)
         {
-            using (var connection = Helpers.NewConnection())
-            {
-
-                SqlCommand command = new SqlCommand($"update Accounts set Amount = Amount + {transaction.Amount} where Id = '{id}'", connection);
-                connection.Open();
-                if (command.ExecuteNonQuery() != 1)
-                    return BadRequest("Could not update account amount");
-
-                command = new SqlCommand($"INSERT INTO Transactions (Id, Amount, Date, AccountId) VALUES ('{Guid.NewGuid()}', {transaction.Amount}, '{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}', '{id}')", connection);
-                if (command.ExecuteNonQuery() != 1)
-                    return BadRequest("Could not insert the transaction");
-
-                return Ok();
-            }
+            _transactionRepository.AddTransaction(id, transaction);
+            return Ok();
         }
     }
 }
